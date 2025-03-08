c24210f8f057101623b0d45a96d1948ea4599de7
Passerby-A-study <780551528@qq.com>
2025-03-08 21:50:10 +0800

    Delete weibo directory

--- weibo/run_all.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# 依次运行四个 Python 文件
-subprocess.run(['python', 'resou_rank_url_title_spider.py'])
-subprocess.run(['python', 'postID_spider.py'])
-subprocess.run(['python', 'poster_spider.py'])
-subprocess.run(['python', 'commend_spider.py'])
